467595bb9ce99449f3356043f56f7b549808d60b
Zodiax37 <98854282+Zodiax37@users.noreply.github.com>
2025-02-18 03:23:11 +0000

    First COmmit withnoHack

--- src/app.js
+++ src/app.js
@@ -5,7 +5,28 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extns = ['com', 'es', 'net', 'us', 'io'];
+
+function Genetator(params) {
+  
+}
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  for (let pro of pronoun){
+    for(let ad of adj){
+      for (let nou of noun){
+        for(let ext of extns)
+        {
+          let subString= `${pro}${ad}${nou}.${ext}`
+        console.log(subString)
+        }
+      }
+    }
+  }
+
 };
